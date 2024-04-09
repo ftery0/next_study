de3ff60f5a1fbdd461e40324eafdbd457b28d694
해준 <127281057+ftery0@users.noreply.github.com>
2024-04-10 00:34:51 +0900

    type

--- src/Components/Main/main.tsx
+++ src/Components/Main/main.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+import {moview} from "src/types/movies/movies.types"
 const url = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 const getMoveies = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 100));
   const respoense = await fetch(url);
   const json = await respoense.json();
   return json;
@@ -9,6 +11,14 @@ const getMoveies = async () => {
 
 const Main = async () => {
   const Movies = await getMoveies();
-  return <div>{JSON.stringify(Movies)}</div>;
+  return (
+    <div>
+      {Movies.map((movie : moview) => ( 
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 };
 export default Main;
