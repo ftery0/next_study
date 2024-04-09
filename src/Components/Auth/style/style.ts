04ac21c27eb1a4bb95f6f3a042d7e326f5b9559d
해준 <127281057+ftery0@users.noreply.github.com>
2024-04-09 22:54:40 +0900

    공부1

--- src/Components/Auth/style/style.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Main = styled.div`
-width: 100vw;
-height: 100vh;
-
-`
--- src/Components/Main/main.tsx
+++ src/Components/Main/main.tsx
@@ -1,4 +1,14 @@
-const Main = () => {
-  return <></>;
+const url = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+const getMoveies = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const respoense = await fetch(url);
+  const json = await respoense.json();
+  return json;
+};
+
+const Main = async () => {
+  const Movies = await getMoveies();
+  return <div>{JSON.stringify(Movies)}</div>;
 };
 export default Main;
