4263c339e0779a103025f1dd034701fe4d1c013d
ftery0 <ftery030215@naver.com>
2024-08-19 21:46:31 +0900

    STUDYYY

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,10 +1,8 @@
-import Navgiton from "src/Components/nav/index";
 import Main from "src/Components/Main/main";
 
 export default function Home() {
   return (
     <>
-      <Navgiton />
       <h1>wlecome Home</h1>
       <Main />
     </>
